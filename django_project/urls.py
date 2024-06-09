c3b3e665a93f74e64d4613753ea9bbaca8c1e663
vikasMotwani <motwanivikas987@gmail.com>
2024-06-09 12:23:02 -0500

    Removed empty url

--- django_project/urls.py
+++ django_project/urls.py
@@ -21,6 +21,5 @@ from users import views as user_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
-    path('', include('blog.urls'),),
     path('register/', user_views.register, name='register'),
 ]
